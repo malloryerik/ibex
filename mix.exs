deae0aeaa4b3e0a3b71b612b0067983a3202771f
merik <malloryerik@gmail.com>
2024-02-11 19:09:47 +0900

    add exdoc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,18 @@ defmodule Ibex.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Ibex",
+      source_url: "https://github.com/malloryerik/ibex",
+      homepage_url: "https://github.com/malloryerik/ibex",
+      docs: [
+        # The main page in the docs
+        main: "Ibex"
+        # logo: "path/to/logo.png",
+        # extras: ["README.md"]
+      ]
     ]
   end
 
@@ -24,6 +35,7 @@ defmodule Ibex.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
   end
 end
